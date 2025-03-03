db515c7311123197543266d4cb71c3f0b9744308
Joel Barba <joel.barba@unity3d.com>
2025-03-03 09:06:47 -0600

    Changing fuinction signature of generate to use a tuple because ref code was unclear and did not interact with the property definition very well.

--- Packages/com.usp.metaaddressables/Editor/MetaAddressables.cs
+++ Packages/com.usp.metaaddressables/Editor/MetaAddressables.cs
@@ -37,30 +37,30 @@ namespace USP.MetaAddressables
             return MetaFile.Read(assetImporter, UserDataKey, factory.Create);
         }
 
-        private static AddressableAssetGroup Generate(AddressableAssetSettings settings,
-            ref GroupData groupData)
+        public static (AddressableAssetGroup, GroupData) Generate(GroupData groupData, AddressableAssetSettings settings)
         {
             AddressableAssetGroup group;
 
             // If there is no group GUID assigned to the userdata,
             // then either we haven't found a prexisting group that matches,
-            // or there is no prexisting one that matches.
+            // or the group data represents a template and there is no prexisting group that matches.
             if (!string.IsNullOrEmpty(groupData.Guid))
             {
                 // Attempt to find a group associated with the guid.
-                // If there was a group found that was associated with the guid, thenG
+                // If there was a group found that was associated with the guid, then:
                 if (AddressablesLookup.GroupsAndHashesByGuids.TryGetValue(groupData.Guid, out group))
                 {
-                    // Return the found group. Do nothing else.
-                    return group;
+                    // Return the found group and no group data since there is no modifications made to it.
+                    // Do nothing else.
+                    return (group, null);
                 }
-
-                // Otherwise, there was no group found associated with the guid.
-
-                // Attempt to find another existing group or create a new group.
             }
 
-            // Get the hash code for the group.
+            // Otherwise, there was no group found associated with the guid, and/or no guid exists.
+
+            // Attempt to find an existing group with the identical properties, or create a one from the group data.
+
+            // Get the hash code for the group (which hashed the values of the properties).
             int hash = groupData.GetHashCode();
 
             // Attempt to find a list of groups that are associated with the group data.
@@ -76,33 +76,32 @@ namespace USP.MetaAddressables
                 // Create a new group data instance based off of the group.
                 groupData = new GroupData(group);
 
-                return group;
+                return (group, groupData);
             }
 
             // Otherwise, there is no group associated with the hash.
 
-            // Create a new list of groups
-            groupList = new List<AddressableAssetGroup>();
-
-            // Associate the list with the hash. 
-            AddressablesLookup.GroupsByPropertyHash.Add(hash, groupList);
-
+            // Add the group to the Addressables system (and AddressablesLookup)
             group = GroupData.Create(settings, groupData);
 
-            groupList.Add(group);
-
-            return group;
+            // Return the found group and no group data since there is no modifications made to it.
+            return (group, null);
         }
 
-        public static void Generate(UserData userData)
+        public static AddressableAssetEntry Generate(ref UserData userData, AddressableAssetSettings settings)
         {
-            var settings = AddressableAssetSettingsDefaultObject.Settings;
+            if (settings == null)
+            {
+                return null;
+            }
 
-            var groupData = userData.Group;
-            AddressableAssetGroup group = Generate(settings, ref groupData);
-            userData.Group = groupData;
+            (AddressableAssetGroup group, GroupData groupData) = Generate(userData.Group, settings);
+            if (groupData != null)
+            {
+                userData.Group = groupData;
+            }
 
-            AssetData.CreateOrMove(settings, group, userData.Asset);
+            return AssetData.CreateOrMove(settings, group, userData.Asset);
         }
 
         public static void Clear(string assetFilePath)
