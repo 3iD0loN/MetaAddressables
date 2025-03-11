49c0495ad649e16b6c00577d2635c50240ca9e14
Joel Barba <joel.barba@unity3d.com>
2025-03-10 22:42:32 -0500

    Remove unused namespaces.

--- Packages/com.usp.metaaddressables/Editor/Comparer/IEqualityComparer.cs
+++ Packages/com.usp.metaaddressables/Editor/Comparer/IEqualityComparer.cs
@@ -1,13 +1,3 @@
-using DocumentFormat.OpenXml.Drawing.Charts;
-using System;
-using System.Collections.Generic;
-using System.Linq.Expressions;
-
-using UnityEngine.ResourceManagement.Util;
-
-using static UnityEditor.AddressableAssets.Settings.GroupSchemas.BundledAssetGroupSchema;
-using static UnityEngine.GraphicsBuffer;
-
 namespace USP.MetaAddressables
 {
     public interface IEqualityComparer
