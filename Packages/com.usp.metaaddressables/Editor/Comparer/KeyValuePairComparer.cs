b8e3efa42ff64ff9f43a1d7d4faf21be1c5ec9c3
Joel Barba <joel.barba@unity3d.com>
2025-03-20 05:28:25 -0500

    Adding a version of KeyValueComparer that only compares the keys.

--- Packages/com.usp.metaaddressables/Editor/Comparer/KeyValuePairComparer.cs
+++ Packages/com.usp.metaaddressables/Editor/Comparer/KeyValuePairComparer.cs
@@ -4,6 +4,11 @@ namespace USP.MetaAddressables
 {
     public class KeyValuePairComparer<K, V> : PropertyComparer<KeyValuePair<K, V>>
     {
+        public KeyValuePairComparer(IPropertyComparer keyComparer) :
+            base((x => x.Key, keyComparer))
+        {
+        }
+
         public KeyValuePairComparer(IPropertyComparer keyComparer, IPropertyComparer valueComparer) :
             base((x => x.Key, keyComparer),
                 (x => x.Value, valueComparer))
