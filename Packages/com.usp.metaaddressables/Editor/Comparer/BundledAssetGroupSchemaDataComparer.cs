3e6aaddd841eb944ab7a48aab9fa0952200fa054
Joel Barba <joel.barba@unity3d.com>
2025-03-28 02:52:10 -0500

    Adding a comparer for PropertyValueReference since there was an assumption being made that all objects have a direct class relationship. There was a reflection error.,

--- Packages/com.usp.metaaddressables/Editor/Comparer/BundledAssetGroupSchemaDataComparer.cs
+++ Packages/com.usp.metaaddressables/Editor/Comparer/BundledAssetGroupSchemaDataComparer.cs
@@ -1,5 +1,24 @@
 namespace USP.MetaAddressables
 {
+    using UnityEditor.AddressableAssets.Settings;
+    using UnityEngine.ResourceManagement.Util;
+
+    public class ProfileValueReferenceComparer : PropertyComparer<ProfileValueReference>
+    {
+        public ProfileValueReferenceComparer() :
+            base((x => x.Id, ObjectComparer.Default))
+        {
+        }
+    }
+
+    public class SerializedTypeComparer : PropertyComparer<SerializedType>
+    {
+        public SerializedTypeComparer() :
+            base((x => x.Value, ObjectComparer.Default))
+        {
+        }
+    }
+
     public class BundledAssetGroupSchemaDataComparer : PropertyComparer<MetaAddressables.BundledAssetGroupSchemaData>
     {
         public BundledAssetGroupSchemaDataComparer() :
@@ -11,7 +30,7 @@ namespace USP.MetaAddressables
                 (x => x.InternalIdNamingMode, ObjectComparer.Default),
                 (x => x.CacheClearBehavior, ObjectComparer.Default),
                 (x => x.IncludeInBuild, ObjectComparer.Default),
-                (x => x.BundledAssetProviderType.Value, ObjectComparer.Default),
+                (x => x.BundledAssetProviderType, new SerializedTypeComparer()),
                 (x => x.ForceUniqueProvider, ObjectComparer.Default),
                 (x => x.UseAssetBundleCache, ObjectComparer.Default),
                 (x => x.UseAssetBundleCrc, ObjectComparer.Default),
@@ -21,10 +40,10 @@ namespace USP.MetaAddressables
                 (x => x.ChunkedTransfer, ObjectComparer.Default),
                 (x => x.RedirectLimit, ObjectComparer.Default),
                 (x => x.RetryCount, ObjectComparer.Default),
-                (x => x.BuildPath.Id, ObjectComparer.Default),
-                (x => x.LoadPath.Id, ObjectComparer.Default),
+                (x => x.BuildPath, new ProfileValueReferenceComparer()),
+                (x => x.LoadPath, new ProfileValueReferenceComparer()),
                 (x => x.BundleMode, ObjectComparer.Default),
-                (x => x.AssetBundleProviderType.Value, ObjectComparer.Default),
+                (x => x.AssetBundleProviderType, new SerializedTypeComparer()),
                 (x => x.UseDefaultSchemaSettings, ObjectComparer.Default),
                 (x => x.SelectedPathPairIndex, ObjectComparer.Default),
                 (x => x.BundleNaming, ObjectComparer.Default),
